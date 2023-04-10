e3d1e30b3833448dadccfc291f35cebca62cb20d
marciscool1h <40239906+marciscool1h@users.noreply.github.com>
2023-04-10 18:11:13 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -35,8 +35,6 @@ echo "Enter your choise [1/2/3] "; read
 case $REPLY in
 1)
 apt-get install sudo
-wget -qO gotty.tar.gz https://github.com/yudai/gotty/releases/latest/download/gotty_linux_amd64.tar.gz
-sudo tar xf gotty.tar.gz -C /usr/local/bin
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 echo -e "${GREEN}PufferPanel has been installed! To use it, go to ${YELLOW}/etc/pufferpanel/config.json${GREEN}, find ${YELLOW}\"host\"${GREEN} and replace 8080 with your server port"
@@ -44,7 +42,6 @@ echo -e "${GREEN}After that, type in console ${YELLOW}sudo systemctl enable puff
 echo -e "${GREEN}To add user, open goTTY and enter ${YELLOW}pufferpanel user add${GREEN} and follow the instructions"
 echo -e "${GREEN}Script will turn on goTTY now${NC}"
 echo "Enter your server port:"; read
-gotty -w --once -p $REPLY bash
 ;;
 2)
 apt remove pufferpanel
